77537b865e669aeefe95bf9b0033038f77edf07d
tomymaritano <tomymaritano@gmail.com>
2025-06-23 20:48:24 -0300

    feat:(hero) hero section

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export default function HeroSection() {
+    return (
+        <section className="w-full py-20 grid grid-cols-12 ">
+            {/* Espacio vacío solo en desktop */}
+            <div className="hidden lg:block col-span-7"></div>
+
+            {/* Bloque derecho */}
+            <div className="col-span-12 lg:col-span-5    flex flex-col items-end lg:items-end text-right md:text-right md:items-right lg:text-right sm:items-right  space-y-4">
+                <p className="text-[18px] sm:text-[22px] lg:text-[24px] tracking-wide text-black/60 font-grotesk">
+                    (Based in Buenos Aires, Working Worldwide)
+                </p>
+                <h1 className="text-[28px] sm:text-[28px] lg:text-[40px] leading-snug font-grotesk font-medium text-black max-w-3xl">
+                    SOMOS UN ESTUDIO CREATIVO INTEGRAL. CONVERTIMOS IDEAS EN MARCAS, ESPACIOS Y EXPERIENCIAS.
+                </h1>
+            </div>
+        </section>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Navbar from './components/Navbar';
 import GridWrapper from './components/GridWrapper';
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
   return (
     <>
       {/* Nav global */}
-      <Navbar />
-
       {/* Main content dentro del grid */}
       <GridWrapper className="py-8">
         {/* Usá col-span para controlar ancho */}
         <main className="col-span-12 flex flex-col gap-8 items-center sm:items-start">
+      <Navbar />
+          <HeroSection />
 
 
           <div className="flex gap-4 items-center flex-col sm:flex-row">
