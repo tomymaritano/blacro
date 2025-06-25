4f7f00fa8cb8854c88592dc605ddabc2c9891a04
tomymaritano <tomymaritano@gmail.com>
2025-06-25 01:40:29 -0300

    fix: projectcard width

--- src/app/project/ProjectCardCarouselRow.tsx
+++ src/app/project/ProjectCardCarouselRow.tsx
@@ -4,22 +4,18 @@ import { featuredProjects } from "../../../data/projects";
 import ProjectCardCarousel from "./ProjectCardCarousel";
 
 export default function ProjectCarouselRow() {
-    // Duplicar el array para que sean dos secuencias consecutivas
-    const loopProjects = [...featuredProjects, ...featuredProjects, ...featuredProjects, ...featuredProjects];
+  return (
+    <div className="relative w-full overflow-x-hidden">
+      {/* Gradientes */}
+      <div className="pointer-events-none absolute left-0 top-0 h-full w-24 z-10"></div>
+      <div className="pointer-events-none absolute right-0 top-0 h-full w-24  z-10"></div>
 
-    return (
-        <div className="relative w-full ">
-            {/* Gradiente lateral izquierda */}
-            <div className="pointer-events-none absolute left-0 top-0 h-full w-24 z-10 "></div>
-            {/* Gradiente lateral derecha */}
-            <div className="pointer-events-none absolute right-0 top-0 h-full w-24 z-10"></div>
-
-            {/* Track */}
-            <div className="flex flex-row space-x-8 animate-marquee-infinite">
-                {loopProjects.map((project, index) => (
-                    <ProjectCardCarousel key={index} {...project} />
-                ))}
-            </div>
-        </div>
-    );
+      {/* Track marquee */}
+      <div className="flex animate-marquee-infinite space-x-8 w-[200%]">
+        {featuredProjects.concat(featuredProjects).map((project, index) => (
+          <ProjectCardCarousel key={index} {...project} />
+        ))}
+      </div>
+    </div>
+  );
 }
